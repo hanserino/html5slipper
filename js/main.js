5ef9a33da51ad5e45f8a117607a09cfccf6a896f
Timbo Slice <me@timboslice.co>
2014-02-05 10:47:06 +0100

    updated commenting to jsdoc format

--- js/main.js
+++ js/main.js
@@ -1,25 +1,35 @@
-/*  ================================================================================
- DEFAULT PARAMETERS
- ================================================================================  */
+/**
+ * GLOBAL PARAMETERS
+ * Define some global parameters
+ * @type {number}
+ */
 var mobileWidth = 650,
 	defaultAnimationSpeed = 300,
+	deviceSize = null,
 	smallClass = 'small',
 	mediumClass = 'medium',
 	largeClass = 'large';
 
 
-/*  ================================================================================
- CHECKS AND STUFF
- ================================================================================  */
+/**
+ * Checks window width on resize and sets deviceSize parameter accordingly
+ */
 function onResize() {
 	var windowWidth = $(window).width();
 	if (windowWidth <= mobileWidth) {
 		//Small window
+		deviceSize = smallClass;
 	}
 	else {
 		//Big window
+		deviceSize = mediumClass;
 	}
 }
+
+
+/**
+ * Checks window width on scroll and triggers necessary actions when pixel positions are reached
+ */
 function onScroll() {
 	var h = $(this).scrollTop();
 	if (h >= 200) {
@@ -30,6 +40,9 @@ function onScroll() {
 	}
 }
 
+/**
+ * Touch Device specific actions
+ */
 // Are we dealing with a touch device?
 if (Modernizr.touch) {
 	//If so, put your touch script here
@@ -39,28 +52,34 @@ else {
 }
 
 
-/*  ================================================================================
- DOCUMENT READY
- ================================================================================  */
+/**
+ * DOCUMENT READY
+ * Actions to perform when the document is ready
+ */
 $(document).ready(function() {
 
+	// Document ready actions here.
+
 });
 
-/*  ================================================================================
- WINDOW LOAD
- ================================================================================  */
+/**
+ * WINDOW LOAD
+ * Actions to perform when all resources are loaded
+ */
 $(window).load(function() {
 
+	// Window load ready actions here.
+
 });
 
 
-/*  ================================================================================
-    ON RESIZE
- ================================================================================  */
+/**
+ * WINDOW EVENTS
+ * Map window events to functions
+ */
+
+// Window Resize	========================================
 $(window).resize(onResize);
 
-
-/*  ================================================================================
- ON SCROLL
- ================================================================================  */
+// Window Scroll	========================================
 $(window).scroll(onScroll);
